421c1155e0ccd3a8a96f8518bc297cee38d173ae
jordanmefujie <mjordanlonpo@gmail.com>
2023-11-24 13:02:05 -0800

    0. Create a file, tmp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# This function creates a file tmp
+
+file { '/tmp/school':
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
