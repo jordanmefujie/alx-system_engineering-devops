ec8c7e4caac2e302867706b56717c9d3e4307812
jordanmefujie <mjordanlonpo@gmail.com>
2023-11-25 12:13:32 -0800

    Updates

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,29 +1,18 @@
 #!/usr/bin/env bash
 # using puppet to connect without password
 
-# Ensure the directory ~/.ssh exists
-file { '/home/ubuntu/.ssh':
-  ensure => directory,
+file { '/etc/ssh/ssh_config':
+  ensure => present,
 }
 
-# Ensure the SSH client configuration file exists
-file { '/home/ubuntu/.ssh/config':
-  ensure  => present,
-  mode    => '0600',
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  content => template('2-ssh_config/ssh_config.erb'),
-}
-
-# Configure the SSH client
 file_line { 'Turn off passwd auth':
-  path  => '/home/ubuntu/.ssh/config',
+  path  => '/etc/ssh/ssh_config',
   line  => 'PasswordAuthentication no',
   match => '^#PasswordAuthentication',
 }
 
 file_line { 'Declare identity file':
-  path  => '/home/ubuntu/.ssh/config',
+  path  => '/etc/ssh/ssh_config',
   line  => 'IdentityFile ~/.ssh/school',
   match => '^#IdentityFile',
 }
